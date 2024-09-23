b97be5ff131b22ee92b66ab48eb1c26ea2ddb506
Lucas Sena <159453926+01Lucasena@users.noreply.github.com>
2024-09-23 14:25:49 +0000

    update

--- project/models/pessoa_juridica.py
+++ project/models/pessoa_juridica.py
@@ -1,6 +1,10 @@
 from abc import ABC
+from project.models.endereco import Endereco
 from project.models.pessoa import Pessoa
 
-class Pessoa_juridica(ABC):
-    def __init__(self) -> None:
-        super().__init__()
+class Pessoa_juridica(Pessoa,ABC):
+    def __init__(self,cnpj: str, inscricao_estadual: str, nome: str, telefone: str, email: str, endereco: Endereco) -> None:
+        super().__init__(nome, telefone, email, endereco)
+    
+        self.cnpj = cnpj
+        self.inscricao_estadual = inscricao_estadual
