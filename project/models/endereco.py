29a91dbbadca5b66eb20c0c04abbe04fdacb136e
Lucas Sena <159453926+01Lucasena@users.noreply.github.com>
2024-09-27 15:06:22 +0000

    update

--- project/models/endereco.py
+++ project/models/endereco.py
@@ -3,28 +3,27 @@ from project.models.enums.unidade_federativa import Unidade_federativa
 class Endereco:
     def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str, unidade_federativa: Unidade_federativa) -> None:
         
-        if not numero:
-            raise ValueError("Número não deve ficar vazio.")
+        if not numero.strip():
+            raise TypeError("Número não deve ficar vazio.")
 
         if not self._is_valid(numero):
-            raise ValueError("Apenas números são permitidos.")
+            raise TypeError("Apenas números são permitidos.")
         
-        if not logradouro:
-            raise ValueError("Logradouro não deve ficar vazio.")
+        if not logradouro.strip():
+            raise TypeError("Logradouro não deve ficar vazio.")
         
-        if not complemento:
-            raise ValueError("Complemento não deve ficar vazio.")
+        if not complemento.strip():
+            raise TypeError("Complemento não deve ficar vazio.")
         
         if not self._is_valid(cep):
-            raise ValueError("Apenas números são permitidos.")
+            raise TypeError("Apenas números são permitidos.")
         
-        if not cidade:
-            raise ValueError("Cidade não deve ficar vazio.")
+        if not cidade.strip():
+            raise TypeError("Cidade não deve ficar vazio.")
         
         if self._is_valid(cidade):
-            raise ValueError("Caracteres inválidos foram inseridos.")
+            raise TypeError("Caracteres inválidos foram inseridos.")
         
-
         self.logradouro = logradouro
         self.numero = numero
         self.complemento = complemento
--- project/tests/test_endereco.py
+++ project/tests/test_endereco.py
@@ -11,42 +11,44 @@ def test_endereco_logradouro_valido(endereco_valido):
     assert endereco_valido.logradouro == "Rua A"
 
 def test_endereco_logradouro_vazio_retorna_mensagem(endereco_valido):
-    with pytest.raises(ValueError,match="Logradouro não deve ficar vazio."):
+    with pytest.raises(TypeError,match="Logradouro não deve ficar vazio."):
         Endereco("","01","Apt 02","0000","Salvador", Unidade_federativa.BAHIA)
     
 def test_endereco_numero_valido(endereco_valido):
     assert endereco_valido.numero == "01"
 
 def test_endereco_numero_vazio_retorna_mensagem(endereco_valido):
-    with pytest.raises(ValueError,match="Número não deve ficar vazio."):
+    with pytest.raises(TypeError,match="Número não deve ficar vazio."):
         Endereco("Rua A","","Apt 02","0000","Salvador", Unidade_federativa.BAHIA)
 
 def test_endereco_caracteres_numero_retorna_mensagem(endereco_valido):
-    with pytest.raises(ValueError,match="Apenas números são permitidos."):
+    with pytest.raises(TypeError,match="Apenas números são permitidos."):
         Endereco("Rua A","abc","Apt 02","0000","Salvador", Unidade_federativa.BAHIA)
 
 def test_endereco_complemento_valido(endereco_valido):
     assert endereco_valido.complemento == "Apt 02"
 
 def test_endereco_complemento_vazio_retorna_mensagem(endereco_valido):
-    with pytest.raises(ValueError,match="Complemento não deve ficar vazio."):
+    with pytest.raises(TypeError,match="Complemento não deve ficar vazio."):
         Endereco("Rua A","01","","0000","Salvador", Unidade_federativa.BAHIA)
 
 def test_endereco_cep_valido(endereco_valido):
     assert endereco_valido.cep == "0000"
 
 def test_endereco_caracteres_cep_retorna_menssagem(endereco_valido):
-    with pytest.raises(ValueError,match="Apenas números são permitidos."):
+    with pytest.raises(TypeError,match="Apenas números são permitidos."):
         Endereco("Rua A","01","Apt 02","abc","Salvador", Unidade_federativa.BAHIA)
 
 def test_endereco_cidade_valido(endereco_valido):
     assert endereco_valido.cidade == "Salvador"
 
 def test_endereco_cidade_vazio_retorna_mensagem(endereco_valido):
-    with pytest.raises(ValueError,match="Cidade não deve ficar vazio."):
+    with pytest.raises(TypeError,match="Cidade não deve ficar vazio."):
         Endereco("Rua A","01","Apt 02","0000","", Unidade_federativa.BAHIA)
 
 def test_endereco_cidade_caracteres_retorna_menssagem(endereco_valido):
-    with pytest.raises(ValueError,match="Caracteres inválidos foram inseridos."):
+    with pytest.raises(TypeError,match="Caracteres inválidos foram inseridos."):
         Endereco("Rua A","01","Apt 02","0000","123", Unidade_federativa.BAHIA)
 
+def test_endereco_unidade_federativa_valida(endereco_valido):
+    assert endereco_valido.unidade_federativa == Unidade_federativa.BAHIA
